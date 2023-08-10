4fb5e8c4cc7ce4d85458475f89d0ac256463e5d9
samsarj <samsarj@gmail.com>
2023-08-10 15:11:32 +0100

    first commit

---
+++ OnAirViewer.py
@@ -0,0 +1,10 @@
+import os
+
+rundownID = input("Please enter Rundown ID: ")
+# print("Rundown ID: ", rundownID)
+
+url = 'https://rundowncreator.com/surrey/OnAirTimerDisplay.php?RundownID='+rundownID
+
+print("Opening ", url)
+
+os.system('"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe" --window-position=-1600,0 --kiosk --user-data-dir=c:/onairviewer --app='+url)
